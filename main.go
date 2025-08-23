d69a57f28c6ef10007b5e1a47e9fd625716db62b
杨定龙 <yangdinglong@192.168.5.27>
2025-08-23 20:04:56 +0800

    xxx

--- main.go
+++ main.go
@@ -10,13 +10,8 @@ var s = "fuck man"
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-	array := [2]int{1, 2}
-	b := &array
-	b[0] = 2
-	s := array[1:]
-	c := s
-	c[0] = 1
-	fmt.Println(c)
+	var s1 []int = make([]int, 0)
+	fmt.Println(s1)
 
 }
 
